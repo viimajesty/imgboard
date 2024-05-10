0137f0508534cf70aaa7bf94cc9b5f4d30ea0ef1
Vivek Kadre <vivekkadre7@gmail.com>
2024-05-10 14:31:44 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const fs = require("fs")
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+app.get('/admin', (req, res) => {
+  res.sendFile(join(__dirname, 'admin.html'));
+})
+//set static directory
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+  socket.on("test", data => {
+    console.log("Test", data);
+  });
+
+  socket.on("newImage", (data => {
+    let url = data.url;
+    let alt = data.alt;
+    let name = data.name;
+    console.log(url, alt)
+    //append to ./public/forReview.json
+    addImage(url, alt, "forReview.json", data.sessionID, name)
+
+  }))
+
+  socket.on("accept", (data => {
+    let url = data.url,
+      alt = data.alt,
+      name = data.name;
+    console.log(url, alt, name)
+    //append to ./public/images.json
+    addImage(url, alt, "images.json", data.id, name)
+    removeImage(data.id, "forReview.json")
+  }))
+
+  socket.on("reject", (data => {
+    let url = data.url,
+      alt = data.alt,
+      name = data.name;
+    console.log(url, alt)
+    //remove from ./public/forReview.json
+    removeImage(data.id, "forReview.json", true)
+  }))
+});
+
+function addImage(url, alt, file, sessionID, name) {
+  fs.readFile("./public/" + file, (err, data) => {
+    if (err) throw err;
+    const images = JSON.parse(data);
+    console.log(images)
+    images.push({ "url": url, "alt": alt, "id": sessionID, "name": name });
+    fs.writeFile("./public/" + file, JSON.stringify(images), (err) => {
+      if (err) throw err;
+      console.log("New image added");
+      io.emit("reloadImages", { submitted: true, sessionID: sessionID })
+    });
+  })
+}
+
+function removeImage(id, file, reload) {
+  fs.readFile("./public/" + file, (err, data) => {
+    if (err) throw err;
+    let images = JSON.parse(data);
+    const index = images.findIndex(image => image.id === id);
+    if (index !== -1) {
+      images.splice(index, 1);
+      fs.writeFile("./public/" + file, JSON.stringify(images), (err) => {
+        if (err) throw err;
+        console.log("Image removed");
+        if (reload) {
+          io.emit("reloadImages", { submitted: false })
+        }
+      });
+      // Optionally, you might want to emit an event to notify clients about the removal
+      // io.emit("imageRemoved", { id });
+
+    } else {
+      console.log("Image not found");
+      // Optionally, you might want to handle the case where the image with the provided id is not found
+    }
+  });
+}
+
+
+server.listen(3002, () => {
+  console.log('server running at http://localhost:3002');
+});
